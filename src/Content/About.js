3a30dd1e69311df511f3cc07e4ce7aec2480eab0
Daniela Lins <danielamlins@gmail.com>
2021-02-06 16:21:38 +0100

    changed about me and updated CVs

--- src/Content/About.js
+++ src/Content/About.js
@@ -7,8 +7,9 @@ function About() {
       <p>
         I am a Software Development student, in the midst of a career change. I am currently learning Full-Stack Web-Development and this field is very fascinating to me. I like the idea of solving real user problems with
         practical solutions. Moreover, I am always eager to learn and I like to understand systems thoroughly, so that I get the big picture. </p>
-        <p>My previous experience as a veterinarian helped me develop many skills, such as resilience to overcome critical situations as well as dealing with stress, difficult costumers and many times the need to think
+        <p>Myy career background is very different from IT, since I graduated and worked mostly as a veterinarian before. However, I have always been interested in tech and missed this field in my every-day life. Also, my previous experience as a veterinarian helped me develop many skills that are useful in any field of work, such as resilience to overcome critical situations as well as dealing with stress, difficult costumers and many times the need to think
         outside of the box. I also learned the need of continuing education to keep me up to date.</p>
+        <p>Since I started learning to code, it has been very rewarding. I am very excited in this new path and look forward to it.</p>
     </div>
   );
 }
