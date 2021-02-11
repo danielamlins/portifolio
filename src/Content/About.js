8f5834f8dfa78865d589ada45029184153b5a9d4
Daniela Lins <danielamlins@gmail.com>
2021-02-11 17:44:04 +0100

    removed lorem text

--- src/Content/About.js
+++ src/Content/About.js
@@ -6,28 +6,13 @@ function About() {
   return (
     <div id="about">
       <h2>About me</h2>
+      
       <p>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Assumenda
-        quibusdam inventore at in similique amet vel laboriosam asperiores,
-        expedita ratione maxime obcaecati! Nulla eligendi nesciunt minima enim
-        est, minus at dicta? Rem commodi nostrum eum sed accusantium nihil amet
-        dolor impedit, autem praesentium possimus quod repellat, mollitia
-        sapiente! Voluptatibus, eveniet.
-      </p>
-      <p>
-        Provident natus temporibus officia nisi non iure, eos ullam debitis
-        optio quisquam consectetur numquam vero laborum iste ab illo fuga
-        exercitationem. Sint harum quibusdam non debitis ad veniam illo rerum
-        temporibus quam iusto doloribus, doloremque odio nostrum. Nostrum eum
-        neque ipsa doloremque laudantium, tenetur possimus quisquam, placeat,
-        eius pariatur ea?
-      </p>
-      {/* <p>
         I am a Software Development student, in the midst of a career change. I am currently learning Full-Stack Web-Development and this field is very fascinating to me. I like the idea of solving real user problems with
         practical solutions. Moreover, I am always eager to learn and I like to understand systems thoroughly, so that I get the big picture. </p>
         <p>Myy career background is very different from IT, since I graduated and worked mostly as a veterinarian before. However, I have always been interested in tech and missed this field in my every-day life. Also, my previous experience as a veterinarian helped me develop many skills that are useful in any field of work, such as resilience to overcome critical situations as well as dealing with stress, difficult costumers and many times the need to think
         outside of the box. I also learned the need of continuing education to keep me up to date.</p>
-        <p>Since I started learning to code, it has been very rewarding. I am very excited in this new path and look forward to it.</p> */}
+        <p>Since I started learning to code, it has been very rewarding. I am very excited in this new path and look forward to it.</p>
     </div>
   );
 }
